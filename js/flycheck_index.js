07404fa2157f00b993dddd43a40d1654e16ebf11
Charanjit Singh <charanjit.singh@trantorinc.com>
2019-08-08 18:52:34 +0530

    rebuilding site Thu 08 Aug 2019 06:52:34 PM IST

---
+++ js/flycheck_index.js
@@ -0,0 +1,7 @@
+var hireMeButton = document.getElementById('hire-me-btn');
+var hireMeDialog = document.getElementById('hire-me-dialog');
+hireMeButton.addEventListener('click', function (e) {
+    var btn = e.target;
+    btn.classList.add('expanded');
+});
+//# sourceMappingURL=flycheck_index.js.map
